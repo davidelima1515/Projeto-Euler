5e46736d6e8995126d6b9e6b24b43c259f9f0d7f
Davi <davidelima1515@gmail.com>
2021-11-04 01:39:41 -0300

    Atualizando código para avaliar se número é primo

--- especiais/primo.py
+++ especiais/primo.py
@@ -1,6 +1,34 @@
 def isprime(number):
-    metade = int(number/2)
-    if any([number % n ==0 for n in range(2, number)]):
+    if number == 1:
         return False
+    elif number % 2 == 0:
+        if number == 2:
+            return True
+        else:
+            return False
+    elif number < 100000:
+        if number == 3 or number == 5 or number == 7:
+            return True    
+        elif number % 3 == 0 or number % 5== 0 or number % 7==0:
+            return False
+        elif any([number % n == 0 for n in range(3, int(number/3))]):
+            return False
+        else:
+            return True
     else:
-        return True
+        if number % 3 == 0 or number % 5== 0 or number % 7==0:
+            return False
+        elif any([number % n == 0 for n in range(3, int(number/100))]):
+            return False
+        else:
+            return True
+
+
+
+
+
+
+
+
+
+
