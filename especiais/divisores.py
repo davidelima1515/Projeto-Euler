937988238c8e50aff9a40fc59584865e1ead271f
Davi <davidelima1515@gmail.com>
2021-11-10 13:26:22 -0300

    atualizando para pegar a soma dos divisores

--- especiais/divisores.py
+++ especiais/divisores.py
@@ -1,10 +1,25 @@
 
-def numero_divisores(number):
-    divisores = [num for num in range(1, int(number/2)) if number % num == 0]
-    if number == 3 :
-        return 2
-    if number % 2 == 0:
-            return len(divisores) + 2 
-            
+from especiais.primo import isprime
+
+def soma_divisores(number):
+    if number == 1:
+        return 1
+    if isprime(number):
+        return (1 + number)
+    elif  number % 2 == 0:
+        divisores = [num for num in range(1, int(number/2 + 1)) if number % num == 0]
+        return sum(divisores) 
     else:
-        return len(divisores) + 1
+        divisores = [num for num in range(1, int(number/primeiro_divisor(number) +1)) if number % num == 0]
+        return sum(divisores) 
+
+
+
+def primeiro_divisor(number):
+    num = 3
+    while 1:
+        if number % num == 0:
+            return num
+            break
+        else: 
+            num += 2
